60be3b747fe5c31942f4b6d71eeefa9b38061862
OpticStranger <72882180+OpticStranger@users.noreply.github.com>
2021-03-22 09:51:13 -0700

    Andrews file

---
+++ gamereview/Andrew/app.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+
+gamereview_bp = Blueprint('gamereview', __name__,
+                          template_folder='templates',
+                          static_folder='static', static_url_path='assets')
+
+
+@gamereview_bp.route('/')
+def index():
+    return "Y2021 tri2 Home Site"
