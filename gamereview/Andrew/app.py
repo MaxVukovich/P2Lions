b702e09a11e00e664cc13c39923171a870455e3c
andrewc26 <73131429+andrewc26@users.noreply.github.com>
2021-04-03 08:46:42 -0700

    Andrew's minilab

--- gamereview/Andrew/app.py
+++ gamereview/Andrew/app.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from gamereview.Andrew.foods import Foods
 
 gamereview_bp2 = Blueprint('gamereview2', __name__,
                           template_folder='templates',
@@ -8,3 +9,11 @@ gamereview_bp2 = Blueprint('gamereview2', __name__,
 @gamereview_bp2.route('/')
 def index():
     return render_template("andrewhome.html")
+
+@gamereview_bp2.route('/andrewminilab', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        a = int(request.form.get("series"))
+        foodrecs = Foods(a/a)
+        return render_template("andrewminilab.html", foodrecs=Foods(a))
+    return render_template("andrewminilab.html", foodrecs=Foods(1))
