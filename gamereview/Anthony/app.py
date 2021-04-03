d0b3ee70779252432b2ab11df55134f58595c147
anthonywilsonn <antonwilo360@icloud.com>
2021-04-02 19:25:12 -0700

    anthony mini lab

--- gamereview/Anthony/app.py
+++ gamereview/Anthony/app.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from gamereview.Anthony.sports import Sports
 
 gamereview_bp3 = Blueprint('gamereview3', __name__,
                           template_folder='templates',
@@ -8,3 +9,11 @@ gamereview_bp3 = Blueprint('gamereview3', __name__,
 @gamereview_bp3.route('/')
 def index():
     return render_template("anthonyhome.html")
+
+@gamereview_bp3.route('/anthonyminilab', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        a = int(request.form.get("series"))
+        sportrecs = Sports(a/a)
+        return render_template("anthonyminilab.html", sportrecs=Sports(a))
+    return render_template("anthonyminilab.html", sportrecs=Sports(1))
