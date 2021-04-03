852c3e62eaba6e9a703c89ff43bd4fff5316bda6
OpticStranger <72882180+OpticStranger@users.noreply.github.com>
2021-04-03 03:21:18 -0700

    Ahmad minilab

--- gamereview/Ahmad/app.py
+++ gamereview/Ahmad/app.py
@@ -10,25 +10,11 @@ gamereview_bp1 = Blueprint('gamereview1', __name__,
 def index():
     return render_template("ahmadhome.html")
 
-@gamereview_bp1.route('/select-game', methods=['GET', 'POST'])
+@gamereview_bp1.route('/games', methods=['GET', 'POST'])
 def search():
-    #genres = 'nothing'
-    #if request.method == 'POST':
-    #genres = request.form.getlist('genre')
-    #print(genres)
-    #if 'Romance' and 'SciFi' and 'Nonfiction' and 'Comedy' in genres:
-    #    return render_template("allgenres.html")
-    #if 'Comedy' in genres:
-    #    return render_template("index.html")
-    #if 'Nonfiction' in genres:
-    #    return render_template("index.html")
-    #if 'Romance' in genres:
-    #    return render_template("index.html")
-    #if 'SciFi' in genres:
-    #    return render_template("index.html")
     if request.method == 'POST':
         a = int(request.form.get("series"))
         gamerecs = Games(a/a)
-        return render_template("select-game.html", gamerecs=Games(a))
-    return render_template("select-game.html", gamerecs=Games(1))
+        return render_template("games.html", gamerecs=Games(a))
+    return render_template("games.html", gamerecs=Games(1))
 
