8cccbd0d5cf4be7589f09eec4d370600b5f5f209
andrewc26 <73131429+andrewc26@users.noreply.github.com>
2021-04-27 09:40:40 -0700

    andrew bubblesort

--- gamereview/Andrew/app.py
+++ gamereview/Andrew/app.py
@@ -10,6 +10,7 @@ gamereview_bp2 = Blueprint('gamereview2', __name__,
 def index():
     return render_template("andrewhome.html")
 
+# class lab
 @gamereview_bp2.route('/andrewminilab', methods=['GET', 'POST'])
 def search():
     if request.method == 'POST':
@@ -17,6 +18,7 @@ def search():
         return render_template("andrewminilab.html", foodrecs=Foods(k))
     return render_template("andrewminilab.html", foodrecs=Foods(1))
 
+# bubblesort lab
 @gamereview_bp2.route('/bubblesort')
 def bubbles1():
     return render_template("bubbles1.html")
